5cf58aa7a29edcc89f7d80bcc5df25751ee6014f
cocotv666 <139615474+cocotv666@users.noreply.github.com>
2024-01-24 14:35:14 +0800

    Update Coco_Alsploit_Config.lua

--- Coco_Alsploit_Config.lua
+++ Coco_Alsploit_Config.lua
@@ -1,31 +1,3 @@
-repeat task.wait() until game:IsLoaded()
+shared.AlSploitLoaded = true
 
-local BlacklistedUsers = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlSploit/AlSploit/main/Blacklist"))()
-local RobloxHardWareId = game:GetService("RbxAnalyticsService"):GetClientId()
-local LocalPlayer = game.Players.LocalPlayer
-
-task.spawn(function()
-	for i, v in next, BlacklistedUsers do
-		if v == RobloxHardWareId then
-			task.spawn(function()
-				LocalPlayer:Kick("You are blacklisted from AlSploit. For support, contact godclutcher or join the discord (discord.gg/AlSploit)")
-			end)
-
-			task.spawn(function()
-				shared.AlSploitLoaded = false
-			end)
-
-			task.spawn(function()
-				task.wait(10)
-
-				game:shutdown()
-			end)			
-		end
-
-		if v ~= RobloxHardWareId then
-			shared.AlSploitLoaded = true
-
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/cocotv666/script/main/Coco_Alsploit_Config_MainScript"))()
-		end
-	end
-end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cocotv666/script/main/Coco_Alsploit_Config_MainScript"))()
